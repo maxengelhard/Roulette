6c78604893c7771576e2ce8d9f9bf09945aa664b
maxengelhard <maxvengelhard@gmail.com>
2020-10-09 15:20:51 -0500

    Repeat/Double Will Not Work If The Person Does Not Have The Balance. Bet Amount Buttons Will Now Work After Done Spinning

--- roulette/src/components/Bets/Repeat.js
+++ roulette/src/components/Bets/Repeat.js
@@ -4,7 +4,7 @@ const Repeat = (props) => {
     const repeat = props.repeat ? 'Repeat' : 'Double'
     const click = props.repeat ? () => props.repeatBet() : () => props.double()
     return (
-        <button onClick={click}>
+        <button className='repeat' onClick={click} disabled={props.disableRepeat}>
             {repeat}
         </button>
     )
